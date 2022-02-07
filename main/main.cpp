e6cac61509f0da6c6dd74d39830f002e0377b179
Krithick <56062143+krithick14@users.noreply.github.com>
2022-02-07 15:56:54 +0530

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -10,17 +10,12 @@ using namespace std;
 string exec(string command) {
    char buffer[128];
    string result = "";
-
-   // Open pipe to file
    FILE* pipe = popen(command.c_str(), "r");
    if (!pipe) {
       return "popen failed!";
    }
 
-   // read till end of process:
    while (!feof(pipe)) {
-
-      // use buffer to read and add to result
       if (fgets(buffer, 128, pipe) != NULL)
          result += buffer;
    }
