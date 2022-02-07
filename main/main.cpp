297c164aacd13c19877859c1114eee4be78dae02
Krithick <56062143+krithick14@users.noreply.github.com>
2022-02-07 15:07:10 +0530

    Add files via upload

--- main/main.cpp
+++ main/main.cpp
@@ -5,6 +5,30 @@
 #include "schedule.cpp"
 
 using namespace std;
+
+
+string exec(string command) {
+   char buffer[128];
+   string result = "";
+
+   // Open pipe to file
+   FILE* pipe = popen(command.c_str(), "r");
+   if (!pipe) {
+      return "popen failed!";
+   }
+
+   // read till end of process:
+   while (!feof(pipe)) {
+
+      // use buffer to read and add to result
+      if (fgets(buffer, 128, pipe) != NULL)
+         result += buffer;
+   }
+
+   pclose(pipe);
+   return result;
+}
+
 int main()
 {
     system("clear");
@@ -13,8 +37,11 @@ int main()
 
     system("figlet A d m i n  M e n u");
     cout << "\n";
-    system("systemctl status apache2 | grep -i Active ");
+    cout << "\t"<<exec("systemctl status apache2 | grep -i Active ") << "\n";
+    cout << "\n";
 
+    cout << "\tLocal Ip :" << exec("hostname -I | awk '{print $1}'");
+    
     cout << "\n\n\t1)Start Server\n\n";
     cout << "\t2)Stop Server\n\n";
     cout << "\t3)Restart Server\n\n";
