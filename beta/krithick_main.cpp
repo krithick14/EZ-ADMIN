dcfc9d4132d76f4c0f25bda5284cce4dbe9d333a
Krithick <56062143+krithick14@users.noreply.github.com>
2022-02-07 12:07:35 +0530

    Add files via upload
    Added Option for4 scheduling the start and stop of server

---
+++ beta/krithick_main.cpp
@@ -0,0 +1,85 @@
+#include<iostream>
+#include<fstream>
+#include "backup.cpp"
+#include "schedule.cpp"
+using namespace std;
+int option;
+void main_menu()
+{
+
+    system("figlet A d m i n  M e n u");
+    cout << "\n";
+    system("systemctl status apache2 | grep -i Active ");
+
+    cout << "\n\n\t1)Start Server\n\n";
+    cout << "\t2)Stop Server\n\n";
+    cout << "\t3)Restart Server\n\n";
+    cout << "\t4)Backup Files\n\n";
+    cout << "\t5)Schedule Server\n\n";
+    cout << "\t6)Exit\n\n\n";
+    cout << "\tEnter Your Option: ";
+    cin >> option;
+}
+int main()
+{
+    system("clear");    
+    Server server;
+    main_menu();
+    switch (option)
+    {
+    case 1:
+    {
+        server.start();
+        server.menu();
+        main();
+        break;
+    }
+    
+    case 2:
+    {
+        server.stop();
+        server.menu();
+        main();
+        break;
+    }
+    case 3:
+    {
+        server.restart();
+        server.menu();
+        main();
+        break;
+    }
+    
+    case 4:
+    {
+        Backup obj;
+        obj.bckup();
+        server.menu();
+        main();
+        break;
+    }
+    case 5:
+    {
+        schedule obj;
+        Server server;
+        obj.schedule_menu();
+        server.menu();
+        main();
+        break;
+    }
+    case 6:
+    {
+        cout << "\tEXITTED\n";
+        return 0;
+        break;
+    }
+    
+    default:
+    {
+        cout << "\tInvalid Option\n";
+        server.menu();
+        break;
+    }
+}
+    return 0;
+}
