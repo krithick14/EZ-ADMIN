5eecd3d9900f5521d2f916de1ce7560df7ebd18c
Krithick <56062143+krithick14@users.noreply.github.com>
2022-02-12 22:39:00 +0530

    Update backup.cpp

--- main/backup.cpp
+++ main/backup.cpp
@@ -22,7 +22,7 @@ public:
 
         if (choice == 'y' || choice == 'Y')
         {
-            system("nautilus /home/$USER/backup_server");
+            system("nautilus /home/$USER/backup_server &");
         }
         else
         {
