3a254b7ae36f9f3b473847b5a6b6fa4acabaaf05
Krithick <56062143+krithick14@users.noreply.github.com>
2022-02-18 17:18:34 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,11 @@ function install {
 
     sudo apt install figlet apache2 at xterm
     sudo cp ./ez_admin.html /var/www/html/ez_admin.html
+    g++ main.cpp -o ez-admin
     if [ $? -ne 0 ]; then
         echo "INSTALLATION FAILED !!!"
     fi
-    ./main
+    ./ez-admin
 
 }
 
